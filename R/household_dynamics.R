b4c60c33ffbffbb4d88baee18fc417e80fb0c0fd
nick-gauthier <nicolas.gauthier@asu.edu>
2019-02-21 19:14:24 -0700

    error catching for household dynamics
    makes sure simulation won't fail if everyone dies

--- R/household_dynamics.R
+++ R/household_dynamics.R
@@ -7,12 +7,15 @@
 #' allocate_time(households)
 
 household_dynamics <- function(settlements){
-  unnest(settlements) %>%
-    allocate_time %>%
-    allocate_land %>%
-    farm %>%
-    eat %>%
-    birth_death %>%
-    nest(household:last(everything()), .key = households) %>%
-    mutate(population = map_dbl(households, ~ sum(.$n_occupants)))
+  if(nrow(settlements) > 0){
+    settlements %>%
+      unnest(households) %>%
+      allocate_time %>%
+      allocate_land %>%
+      farm %>%
+      eat %>%
+      population_dynamics %>%
+      nest(household:last(everything()), .key = households) %>%
+      mutate(population = map_dbl(households, ~ sum(.$occupants)))
+    } else {settlements}
 }
