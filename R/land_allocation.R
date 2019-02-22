ec8516efa02d24721bb12cfd74d75caf57a7123f
nick-gauthier <nicolas.gauthier@asu.edu>
2019-02-21 19:15:53 -0700

    update land allocation algorithm so households share scarce land

--- R/land_allocation.R
+++ R/land_allocation.R
@@ -1,4 +1,4 @@
-#' Functions for creating individuals, households, and settlements
+#' Functions for allocating land
 #'
 #'
 #' @param households Tibble of household agents.
@@ -8,13 +8,20 @@
 
 allocate_land <- function(households){
   households %>%
-    mutate(land = calc_land_req(occupants, yield_memory),
-           max_land = max_cultivable_land(laborers, farming_labor, area * arable_proportion * 100)) %>%
-    mutate(land = pmin(land, max_land)) %>%
-    select(-max_land)
+    mutate(max_land = max_cultivable_land(laborers, farming_labor, area * arable * 100, type = 'unlimited'),
+           land_need = pmin(max_land, calc_land_need(occupants, yield_memory)),# land in hectares to support the household, but no more than the laborer can work
+           new_land = if_else(land_need > land, land_need - land, 0)) %>%
+    group_by(settlement) %>%
+    mutate(available_land = area * arable * 100 - sum(land),
+           total_land_need = sum(new_land)) %>%
+    ungroup %>%
+    mutate(land = if_else(total_land_need > available_land,
+                          land + new_land / total_land_need * available_land,
+                          land + new_land)) %>%
+    select(-c(land_need, max_land, new_land, total_land_need, available_land))
 }
 
-calc_land_req <- function(occupants, yield, fallow = TRUE){
+calc_land_need <- function(occupants, yield, fallow = TRUE){
   wheat_req * occupants * (1 + seed_proportion) / yield * ifelse(fallow, 2, 1)
 }
 
@@ -24,4 +31,6 @@ max_cultivable_land <- function(laborers, farming_labor, available_area, fallow 
   if(type == 'step') return(pmin(available_area, potential_area))
   if(type == 'asymptote') return(available_area * (1 - exp(-potential_area / available_area)))
 }
-
+# need to replace the above function with one that changes the value of labor based on the fraction available
+# similar idea, but it requires a different implementation than above because we have multiple households competing
+# for land here. right?
